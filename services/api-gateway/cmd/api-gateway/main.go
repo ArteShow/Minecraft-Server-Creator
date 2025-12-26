6176ed0085b86628a369c04df3507978c7f5ca3c
ArteShow <sokartemax@gmail.com>
2025-12-26 13:47:09 +0100

    add graceful shuttdown

--- services/api-gateway/cmd/api-gateway/main.go
+++ services/api-gateway/cmd/api-gateway/main.go
@@ -1,5 +1,77 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/ArteShow/Minecraft-Server-Creator/services/api-gateway/internal/config"
+	"github.com/ArteShow/Minecraft-Server-Creator/services/api-gateway/internal/middleware"
+)
+
+const (
+	readTimeout  = 10 * time.Second
+	writeTimeout = 10 * time.Second
+	idleTimeou  = 60 * time.Second
+)
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	handler := http.NewServeMux()
+	handler.Handle(
+		"/api/"+cfg.APIVersion+"/api-gateway/health",
+		middleware.LoggingMiddleware(
+			http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+				w.Write([]byte("ok"))
+			}),
+		),
+	)
+
+	//here add proxy handlers TODO
+
+	srv := &http.Server{
+		Addr:    cfg.Port,
+		Handler: handler,
+		ReadTimeout:  readTimeout,
+		WriteTimeout: writeTimeout,
+		IdleTimeout:  idleTimeou,
+	}
+
+	ctx, stop := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt,
+		syscall.SIGTERM,
+	)
+	defer stop()
+
+	go func() {
+		log.Println("gateway running on "+cfg.Port)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("server error: %v", err)
+		}
+	}()
+
+	<-ctx.Done()
+
+	log.Println("graceful shutdown started")
+
+	shutdownCtx, cancel := context.WithTimeout(
+		context.Background(),
+		10*time.Second,
+	)
+	defer cancel()
+
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		log.Printf("shutdown failed: %v", err)
+	}
+
+	log.Println("shutdown complete")
 }
--- services/api-gateway/internal/config/config.go
+++ services/api-gateway/internal/config/config.go
@@ -3,7 +3,7 @@ package config
 import "github.com/ilyakaznacheev/cleanenv"
 
 type Config struct {
-	Port string `env:"API_GATEWAY_PORT" env-default:"800"`
+	Port string `env:"API_GATEWAY_PORT" env-default:":8080"`
 	APIVersion string `env:"API_VERSION" env-default:"v1"`
 }
 
