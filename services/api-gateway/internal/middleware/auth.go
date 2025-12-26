90a33eb7ba27ac2acc0df059161f3490eb4380fe
ArteShow <sokartemax@gmail.com>
2025-12-26 13:31:21 +0100

    add middlewares and proxy creating

--- services/api-gateway/internal/middleware/auth.go
+++ services/api-gateway/internal/middleware/auth.go
@@ -1,1 +1,14 @@
 package middleware
+
+import "net/http"
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//add real jwt token validation TODO
+		if r.Header.Get("Authorization") == "" {
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
--- services/api-gateway/internal/proxy/proxy.go
+++ services/api-gateway/internal/proxy/proxy.go
@@ -1,1 +1,22 @@
 package proxy
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+func NewProxy(target string, rewritePath string) *httputil.ReverseProxy {
+	u, _ := url.Parse(target)
+
+	proxy := httputil.NewSingleHostReverseProxy(u)
+
+	proxy.Director = func(r *http.Request) {
+		r.URL.Scheme = u.Scheme
+		r.URL.Host = u.Host
+		r.Host = u.Host
+		r.URL.Path = u.Path
+	}
+
+	return proxy
+}
