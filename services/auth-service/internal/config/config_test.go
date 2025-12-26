d51fc440956dc6e810e4fe9717d03c1d5a527be5
ArteShow <sokartemax@gmail.com>
2025-12-26 20:52:54 +0100

    fix config package test

--- services/auth-service/internal/config/config_test.go
+++ services/auth-service/internal/config/config_test.go
@@ -8,8 +8,8 @@ func TestRead(t *testing.T) {
 		t.Fatal("Failed to read the config, ", err.Error())
 	}
 
-	if cfg.Port != ":8081" {
-		t.Fatalf("expected port :8081, got: %s", cfg.Port)
+	if cfg.Port != ":8001" {
+		t.Fatalf("expected port :8001, got: %s", cfg.Port)
 	}
 
 	if cfg.JWTSecret != "dev-only" {
