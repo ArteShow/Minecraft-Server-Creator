60660b1bab7be659a013a6a3a98f0739a85ebdfe
ArteShow <sokartemax@gmail.com>
2025-12-29 15:35:13 +0100

    add user repo and id generator packages

--- services/user-service/go.mod
+++ services/user-service/go.mod
@@ -3,6 +3,7 @@ module github.com/ArteShow/Minecraft-Server-Creator/user-service
 go 1.24.4
 
 require (
+	github.com/google/uuid v1.6.0
 	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/lib/pq v1.10.9
 )
--- services/user-service/internal/repository/user_repo.go
+++ services/user-service/internal/repository/user_repo.go
@@ -1,1 +1,86 @@
 package repository
+
+import (
+	"database/sql"
+
+	"github.com/ArteShow/Minecraft-Server-Creator/user-service/internal/database"
+	"github.com/ArteShow/Minecraft-Server-Creator/user-service/pkg/id"
+)
+
+func CreateUser(username, password, email string) error {
+	db, err := database.Connect()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	userID := id.GenerateID()
+
+	_, err = db.Exec(
+		"INSERT INTO users (id, email, password, username) VALUES ($1, $2, $3, $4)",
+		userID, email, password, username,
+	)
+	return err
+}
+
+func DeleteUser(userID string) error {
+	db, err := database.Connect()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	_, err = db.Exec(
+		"DELETE FROM users WHERE id = $1",
+		userID,
+	)
+	return err
+}
+
+func GetID(username string) (string, error) {
+	db, err := database.Connect()
+	if err != nil {
+		return "", err
+	}
+	defer db.Close()
+
+	var userID string
+
+	err = db.QueryRow(
+		"SELECT id FROM users WHERE username = $1",
+		username,
+	).Scan(&userID)
+
+	if err == sql.ErrNoRows {
+		return "", nil
+	}
+	if err != nil {
+		return "", err
+	}
+
+	return userID, nil
+}
+
+func GetPassword(userID string) (string, error) {
+	db, err := database.Connect()
+	if err != nil {
+		return "", err
+	}
+	defer db.Close()
+
+	var password string
+
+	err = db.QueryRow(
+		"SELECT password FROM users WHERE id = $1",
+		userID,
+	).Scan(&password)
+
+	if err == sql.ErrNoRows {
+		return "", nil
+	}
+	if err != nil {
+		return "", err
+	}
+
+	return password, nil
+}
