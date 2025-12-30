c111ad730b7e6f65ab8a7184e65ed6e64a52ab6f
ArteShow <sokartemax@gmail.com>
2025-12-30 18:00:31 +0100

    add start and create handlers

--- services/server-service/go.mod
+++ services/server-service/go.mod
@@ -1,5 +1,11 @@
 module github.com/ArteShow/Minecraft-Server-Creator/services/server-service
 
+<<<<<<< Updated upstream
 go 1.24
+=======
+go 1.24.0
+
+toolchain go1.24.4
+>>>>>>> Stashed changes
 
 require github.com/google/uuid v1.6.0
--- services/server-service/internal/handlers/handlers.go
+++ services/server-service/internal/handlers/handlers.go
@@ -1,1 +1,51 @@
 package handlers
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/ArteShow/Minecraft-Server-Creator/services/server-service/internal/models"
+	getjar "github.com/ArteShow/Minecraft-Server-Creator/services/server-service/pkg/get_jar"
+	idgenerator "github.com/ArteShow/Minecraft-Server-Creator/services/server-service/pkg/id_generator"
+)
+
+func CreateServer(w http.ResponseWriter, r *http.Request) {
+	var req models.CreateServerRequest
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer r.Body.Close()
+
+	err = json.Unmarshal(body, &req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	id := idgenerator.GenerateServerID()
+
+	err = os.Mkdir("./servers"+id, 0755)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = getjar.GetServerJar(req.Version, "./servers/"+id+"/")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	var res models.CreateServerResponse
+	res.ServerID = id
+
+	w.WriteHeader(http.StatusCreated)
+	err = json.NewEncoder(w).Encode(res)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
