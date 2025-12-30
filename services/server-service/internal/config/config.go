32aaa83b4679e0c1b99530abfafd49e155c42694
ArteShow <sokartemax@gmail.com>
2025-12-30 20:35:02 +0100

    add main package and docker support

--- services/server-service/internal/config/config.go
+++ services/server-service/internal/config/config.go
@@ -5,6 +5,7 @@ import "github.com/ilyakaznacheev/cleanenv"
 type Config struct {
 	StartRAM string `env:"START_RAM" env-default:":2G"`
 	RunRAM   string `env:"RUN_RAM" env-default:"4G"`
+	Port     string `env:"SERVER_SERVICE_PORT" env-default:"8003"`
 }
 
 func Read() (*Config, error) {
