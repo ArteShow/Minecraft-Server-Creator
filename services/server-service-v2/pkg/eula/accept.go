eccfd7eb3713c7418f63732a9434504c1f0ac737
ArteShow <sokartemax@gmail.com>
2026-01-03 14:48:09 +0100

    add eula package

--- services/server-service-v2/pkg/eula/accept.go
+++ services/server-service-v2/pkg/eula/accept.go
@@ -1,1 +1,22 @@
 package eula
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func Accept(dir string) error {
+	path := filepath.Join(dir, "eula.txt")
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.WriteString("eula=true")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
