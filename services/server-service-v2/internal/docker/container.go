926e5308ae5df4239215ec02cb261199da969046
ArteShow <sokartemax@gmail.com>
2026-01-11 12:33:44 +0100

    ran go fmt and add docker package

--- services/server-service-v2/internal/docker/container.go
+++ services/server-service-v2/internal/docker/container.go
@@ -1,1 +1,84 @@
 package docker
+
+import (
+	"archive/tar"
+	"bytes"
+	"context"
+	"path"
+	"strings"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/mount"
+)
+
+func (d *DockerService) UploadToVolume(
+	name string,
+	targetPath string,
+	fileName string,
+	data []byte,
+) error {
+	ctx := context.Background()
+
+	targetPath = strings.TrimPrefix(targetPath, "/")
+	fullPath := path.Join(targetPath, fileName)
+
+	resp, err := d.client.ContainerCreate(
+		ctx,
+		&container.Config{
+			Image: "alpine",
+			Cmd:   []string{"sleep", "20"},
+		},
+		&container.HostConfig{
+			Mounts: []mount.Mount{
+				{
+					Type:   mount.TypeVolume,
+					Source: volumeName(name),
+					Target: "/data",
+				},
+			},
+		},
+		nil,
+		nil,
+		"",
+	)
+	if err != nil {
+		return err
+	}
+
+	containerID := resp.ID
+	defer func() {
+		_ = d.client.ContainerRemove(ctx, containerID, container.RemoveOptions{
+			Force: true,
+		})
+	}()
+
+	if err := d.client.ContainerStart(ctx, containerID, container.StartOptions{}); err != nil {
+		return err
+	}
+
+	buf := new(bytes.Buffer)
+	tw := tar.NewWriter(buf)
+
+	hdr := &tar.Header{
+		Name: fullPath,
+		Mode: 0644,
+		Size: int64(len(data)),
+	}
+	if err := tw.WriteHeader(hdr); err != nil {
+		return err
+	}
+	if _, err := tw.Write(data); err != nil {
+		return err
+	}
+	if err := tw.Close(); err != nil {
+		return err
+	}
+
+	return d.client.CopyToContainer(
+		ctx,
+		containerID,
+		"/data",
+		buf,
+		container.CopyToContainerOptions{},
+	)
+}
--- services/server-service-v2/internal/docker/volumes.go
+++ services/server-service-v2/internal/docker/volumes.go
@@ -1,13 +1,9 @@
 package docker
 
 import (
-	"archive/tar"
-	"bytes"
 	"context"
 
-	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/filters"
-	"github.com/docker/docker/api/types/mount"
 	"github.com/docker/docker/api/types/volume"
 	"github.com/docker/docker/client"
 )
@@ -17,16 +13,23 @@ type DockerService struct {
 }
 
 func NewDockerService() (*DockerService, error) {
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	cli, err := client.NewClientWithOpts(
+		client.FromEnv,
+		client.WithAPIVersionNegotiation(),
+	)
 	if err != nil {
 		return nil, err
 	}
 	return &DockerService{client: cli}, nil
 }
 
+func volumeName(name string) string {
+	return "mc_" + name
+}
+
 func (d *DockerService) CreateVolume(name string) error {
 	_, err := d.client.VolumeCreate(context.Background(), volume.CreateOptions{
-		Name: "mc_" + name,
+		Name: volumeName(name),
 		Labels: map[string]string{
 			"project": "minecraft-server",
 		},
@@ -35,13 +38,13 @@ func (d *DockerService) CreateVolume(name string) error {
 }
 
 func (d *DockerService) VolumeExists(name string) (bool, error) {
-	vols, err := d.client.VolumeList(context.Background(), volume.ListOptions{Filters: filters.Args{}})
+	vols, err := d.client.VolumeList(context.Background(), volume.ListOptions{})
 	if err != nil {
 		return false, err
 	}
 
 	for _, v := range vols.Volumes {
-		if v.Name == "mc_"+name {
+		if v.Name == volumeName(name) {
 			return true, nil
 		}
 	}
@@ -52,12 +55,14 @@ func (d *DockerService) ListVolumes() ([]string, error) {
 	args := filters.NewArgs()
 	args.Add("label", "project=minecraft-server")
 
-	vols, err := d.client.VolumeList(context.Background(), volume.ListOptions{Filters: args})
+	vols, err := d.client.VolumeList(context.Background(), volume.ListOptions{
+		Filters: args,
+	})
 	if err != nil {
 		return nil, err
 	}
 
-	names := []string{}
+	names := make([]string, 0, len(vols.Volumes))
 	for _, v := range vols.Volumes {
 		names = append(names, v.Name)
 	}
@@ -65,53 +70,9 @@ func (d *DockerService) ListVolumes() ([]string, error) {
 }
 
 func (d *DockerService) DeleteVolume(name string) error {
-	return d.client.VolumeRemove(context.Background(), "mc_"+name, true)
-}
-
-func (d *DockerService) UploadToVolume(volumeName, targetPath, fileName string, data []byte) error {
-	ctx := context.Background()
-
-	resp, err := d.client.ContainerCreate(ctx, &container.Config{
-		Image: "alpine:latest",
-		Cmd:   []string{"sleep", "60"},
-	}, &container.HostConfig{
-		Mounts: []mount.Mount{
-			{
-				Type:   mount.TypeVolume,
-				Source: volumeName,
-				Target: targetPath,
-			},
-		},
-	}, nil, nil, "")
-	if err != nil {
-		return err
-	}
-	containerID := resp.ID
-
-	defer func() {
-		_ = d.client.ContainerRemove(ctx, containerID, container.RemoveOptions{Force: true})
-	}()
-
-	if err := d.client.ContainerStart(ctx, containerID, container.StartOptions{}); err != nil {
-		return err
-	}
-
-	buf := new(bytes.Buffer)
-	tw := tar.NewWriter(buf)
-	defer tw.Close()
-
-	hdr := &tar.Header{
-		Name: fileName,
-		Mode: 0644,
-		Size: int64(len(data)),
-	}
-	if err := tw.WriteHeader(hdr); err != nil {
-		return err
-	}
-	if _, err := tw.Write(data); err != nil {
-		return err
-	}
-	tw.Close()
-
-	return d.client.CopyToContainer(ctx, containerID, targetPath, buf, container.CopyToContainerOptions{AllowOverwriteDirWithFile: true})
+	return d.client.VolumeRemove(
+		context.Background(),
+		volumeName(name),
+		true,
+	)
 }
