ac2d48100b2e23917f656b46cfe17afe5f974ede
ArteShow <sokartemax@gmail.com>
2026-01-18 11:10:28 +0100

    add jwt key support

--- services/api-gateway/internal/config/config.go
+++ services/api-gateway/internal/config/config.go
@@ -5,6 +5,7 @@ import "github.com/ilyakaznacheev/cleanenv"
 type Config struct {
 	Port string `env:"API_GATEWAY_PORT" env-default:":8000"`
 	APIVersion string `env:"API_VERSION" env-default:"v1"`
+	JWTKey string `env:"JWT_SECRET" env-default:"dev-only"`
 }
 
 func Read() (*Config, error) {
