acf8320aa5356dba717253c7c7db28ddbd9fdd15
ArteShow <sokartemax@gmail.com>
2026-03-18 20:27:33 +0100

    add network repo

--- hub/services/network-service/go.mod
+++ hub/services/network-service/go.mod
@@ -3,6 +3,7 @@ module github.com/ArteShow/Minecraft-Server-Creator/hub/services/network-service
 go 1.24.4
 
 require (
+	github.com/google/uuid v1.6.0
 	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/lib/pq v1.12.0
 	google.golang.org/grpc v1.79.3
--- hub/services/network-service/internal/repository/network_repo.go
+++ hub/services/network-service/internal/repository/network_repo.go
@@ -1,1 +1,24 @@
 package repository
+
+import (
+	"github.com/ArteShow/Minecraft-Server-Creator/hub/services/network-service/internal/database"
+	"github.com/google/uuid"
+)
+
+func CreateServerFuntion(ip string) error {
+	serverID := uuid.NewString()
+
+	db, err := database.Connect()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	_, err = db.Exec(
+		`INSERT INTO hosts (id, ip)
+		 VALUES ($1, $2)`,
+		serverID, ip,
+	)
+
+	return err
+}
