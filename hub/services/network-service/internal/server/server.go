c6b3eaf1f9f9450ba7cd2f1dc80c65995c56946d
ArteShow <sokartemax@gmail.com>
2026-03-18 20:20:37 +0100

    add server package structure

--- hub/services/network-service/internal/server/server.go
+++ hub/services/network-service/internal/server/server.go
@@ -1,1 +1,23 @@
 package server
+
+import (
+	"context"
+
+	network_pb "github.com/ArteShow/Minecraft-Server-Creator/hub/services/network-service/internal/proto"
+)
+
+type Server struct {
+	network_pb.UnimplementedNetworkServiceServer
+}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+func (s *Server) GetServerStatus(_ context.Context, req network_pb.GetServerStatusRequest) (network_pb.GetServerStatusResponse, error) {
+	return network_pb.GetServerStatusResponse{}, nil
+}
+
+func (s *Server) GetServerMetadata(_ context.Context, req network_pb.GetServerMetadataRequest) (network_pb.GetServerMetadataResponse, error) {
+	return network_pb.GetServerMetadataResponse{}, nil
+}
