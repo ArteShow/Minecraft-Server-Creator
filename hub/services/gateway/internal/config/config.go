70af6d27bf7856208d076e4e6637d9b03c45587d
ArteShow <sokartemax@gmail.com>
2026-03-19 13:01:03 +0100

    add config package

--- hub/services/gateway/internal/config/config.go
+++ hub/services/gateway/internal/config/config.go
@@ -1,1 +1,17 @@
 package config
+
+import "github.com/ilyakaznacheev/cleanenv"
+
+type Config struct {
+	Port       string `env:"API_GATEWAY_PORT" env-default:":8000"`
+	APIVersion string `env:"API_VERSION" env-default:"v1"`
+}
+
+func Read() (*Config, error) {
+	cfg := Config{}
+	if err := cleanenv.ReadEnv(&cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
