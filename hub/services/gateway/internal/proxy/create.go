f8413c0de4c869d2311078e0f9732d68b08eb01e
ArteShow <sokartemax@gmail.com>
2026-03-19 12:59:20 +0100

    add proxy package

--- hub/services/gateway/internal/proxy/create.go
+++ hub/services/gateway/internal/proxy/create.go
@@ -1,1 +1,24 @@
 package proxy
+
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+func NewProxy(target string, rewritePath string) *httputil.ReverseProxy {
+	u, err := url.Parse(target)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &httputil.ReverseProxy{
+		Director: func(r *http.Request) {
+			r.URL.Scheme = u.Scheme
+			r.URL.Host = u.Host
+			r.Host = u.Host
+			r.URL.Path = rewritePath
+		},
+	}
+}
