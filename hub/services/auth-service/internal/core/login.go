8b09fdfd07ac87d8ace584d570d7a8bfa932fbbb
ArteShow <sokartemax@gmail.com>
2026-03-23 18:25:24 +0100

    add core package

--- hub/services/auth-service/internal/core/login.go
+++ hub/services/auth-service/internal/core/login.go
@@ -1,1 +1,25 @@
 package core
+
+import (
+	"errors"
+	"time"
+
+	"github.com/ArteShow/Minecraft-Server-Creator/hub/services/auth-service/internal/jwt"
+	"github.com/ArteShow/Minecraft-Server-Creator/hub/services/auth-service/internal/repository"
+)
+
+const JWTTTL = time.Hour*24
+
+func LoginUser(username, password string) (string, error) {
+	userID, ok := repository.CheckUserLogin(username, password)
+	if !ok {
+		return "", errors.New("no user found with username: " + username + " and password: " + password)
+	}
+
+	token, err := jwt.GenerateToken(userID, JWTTTL)
+	if err != nil {
+		return "", err
+	}
+
+	return token, err
+}
--- hub/services/auth-service/internal/core/register.go
+++ hub/services/auth-service/internal/core/register.go
@@ -1,1 +1,12 @@
 package core
+
+import "github.com/ArteShow/Minecraft-Server-Creator/hub/services/auth-service/internal/repository"
+
+func RegisterUser(username, password, email string) (string, error) {
+	userID, err := repository.CreateUser(username, password, email)
+	if err != nil {
+		return "", err
+	}
+
+	return userID, nil
+}
