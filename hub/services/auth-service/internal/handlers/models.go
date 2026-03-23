974d0c256c0a7434e5851774de88a24c89cc9693
ArteShow <sokartemax@gmail.com>
2026-03-23 18:30:47 +0100

    add handlers package

--- hub/services/auth-service/internal/handlers/models.go
+++ hub/services/auth-service/internal/handlers/models.go
@@ -1,1 +1,20 @@
 package handlers
+
+type RegisterUserRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
+}
+
+type RegisterUserResponse struct {
+	UserID string `json:"user_id"`
+}
+
+type LoginUserRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type LoginUserResponse struct {
+	Token string `json:"token"`
+}
--- hub/services/auth-service/internal/handlers/register.go
+++ hub/services/auth-service/internal/handlers/register.go
@@ -1,1 +1,38 @@
 package handlers
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/ArteShow/Minecraft-Server-Creator/hub/services/auth-service/internal/core"
+)
+
+func RegisterUser(w http.ResponseWriter, r *http.Request) {
+	var req RegisterUserRequest
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer r.Body.Close()
+
+	if err = json.Unmarshal(body, &req); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	userID, err := core.RegisterUser(req.Username, req.Password, req.Email)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	resp := RegisterUserResponse{UserID: userID}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	if err = json.NewEncoder(w).Encode(resp); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
