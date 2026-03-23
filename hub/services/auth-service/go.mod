c10899c1f9e4f953828beb422a0d78725f6fcfbd
ArteShow <sokartemax@gmail.com>
2026-03-23 18:21:18 +0100

    add user repo

--- hub/services/auth-service/go.mod
+++ hub/services/auth-service/go.mod
@@ -4,8 +4,10 @@ go 1.25.4
 
 require (
 	github.com/golang-jwt/jwt/v5 v5.3.1
+	github.com/google/uuid v1.6.0
 	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/lib/pq v1.12.0
+	golang.org/x/crypto v0.49.0
 )
 
 require (
--- hub/services/auth-service/internal/repository/user_repo.go
+++ hub/services/auth-service/internal/repository/user_repo.go
@@ -1,1 +1,56 @@
 package repository
+
+import (
+	"time"
+
+	"github.com/ArteShow/Minecraft-Server-Creator/hub/services/auth-service/internal/database"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func CreateUser(username, password, email string) (string, error) {
+	db, err := database.Connect()
+	if err != nil {
+		return "", err
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	id := uuid.New().String()
+
+	_, err = db.Exec(
+		`INSERT INTO users (id, username, password, email, created_at) 
+		 VALUES ($1, $2, $3, $4, $5)`,
+		id,
+		username,
+		string(hash),
+		email,
+		time.Now(),
+	)
+
+	return id, err
+}
+
+func CheckUserLogin(username, password string) (string, bool) {
+	db, err := database.Connect()
+	if err != nil {
+		return "", false
+	}
+
+	var hash, id string
+
+	err = db.QueryRow(
+		`SELECT password, id FROM users WHERE username = $1`,
+		username,
+	).Scan(&hash, &id)
+
+	if err != nil {
+		return "", false
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return id, err == nil
+}
