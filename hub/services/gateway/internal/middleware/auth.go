afc273672b2a2cbad63b56eb8cb987b1c1d9f2c3
ArteShow <sokartemax@gmail.com>
2026-03-30 12:33:26 +0200

    add admin auth middleware

--- hub/services/gateway/internal/middleware/auth.go
+++ hub/services/gateway/internal/middleware/auth.go
@@ -52,3 +52,48 @@ func AuthMiddleware(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+func AdminAuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg, err := config.Read()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return 
+		}
+
+		auth := r.Header.Get("Authorization")
+		if auth == "" {
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		parts := strings.SplitN(auth, " ", 2)
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		token, err := jwt.Parse(parts[1], func(token *jwt.Token) (interface{}, error) {
+			return []byte(cfg.JWTSecret), nil
+		})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return 
+		}
+
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok || !token.Valid {
+			return 
+		}
+
+		adminID, ok := claims["admin_id"].(string)
+		if !ok || adminID == ""{
+			w.WriteHeader(http.StatusUnauthorized)
+			return 
+		}
+
+		r.Header.Set("X-Admin-ID", adminID)
+		r.Header.Set("Authorization", "Bearer "+parts[1])
+		next.ServeHTTP(w, r)
+	})
+}
